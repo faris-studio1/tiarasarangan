6c302294764f6bb55c5db1e1e9b810f405026640
syahrul hidayat <syahrull0505@gmail.com>
2024-11-13 02:56:00 +0700

    Tambahan root

---
+++ app/(pages)/index.js
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+const HomeRedirect = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/User/Beranda");
+  }, [router]);
+
+  return null;
+};
+
+export default HomeRedirect;
