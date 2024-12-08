85316f3f657d2cf080dbfbd11aace564b3be4924
syahrul hidayat <syahrull0505@gmail.com>
2024-12-08 12:11:42 +0700

    perbaikan

--- app/components/(User)/Footer.jsx
+++ app/components/(User)/Footer.jsx
@@ -21,7 +21,7 @@ const FooterComponent = () => {
             </div>
             {/* Call to Action Button */}
             <a
-              href="https://wa.me/6281335623403"
+              href="https://wa.me/6281335623403?text=Halo%2C%20Saya%20ingin%20bertanya.%0AApakah%20ada%20kamar%20yang%20tersedia%20dan%20harga%20kamar%20per-malam%20berapa%3F%0A%0ASaya%20dapat%20informasi%20tentang%20Villa%20Tiara%20dari%20Website%2C%20Terima%20kasih."
               className="inline-block bg-white text-gray-800 px-8 py-3 rounded-lg text-md font-bold shadow-md hover:shadow-lg transform hover:scale-105 transition-all duration-300 ease-in-out"
             >
               Hubungi Kami
